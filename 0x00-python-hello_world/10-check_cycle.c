6c792945082c59fa28cc85f9609b58d4e409cb39
sangwani <mightypz@gmail.com>
2021-11-16 00:20:15 +0200

    first commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *check_cycle - check if linked list has a cycle in it
+ *@list: value check
+ *
+ * Return: 1 if there is a cycle, 0 otherwise.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *x = list;
+	listint_t *y = list;
+
+	if (list == NULL)
+		return (0);
+
+	while (y && y->next)
+	{
+		x = x->next;
+		y = y->next->next;
+
+		if (x == y)
+			return (1);
+	}
+	return (0);
+}
