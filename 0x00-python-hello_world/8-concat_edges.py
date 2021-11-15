c263b6c1efe084ce536e25af2fb48c681baeceb5
sangwani <mightypz@gmail.com>
2021-11-15 23:27:45 +0200

    first commit

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+print('{} {} {}'.format(str[39:66], str[114:118], str[:6]))
