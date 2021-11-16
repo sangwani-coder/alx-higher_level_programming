b56591819a0c8a11723b9efdf549b6c569ae04bd
sangwani <mightypz@gmail.com>
2021-11-16 23:29:29 +0200

    task 6 done

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for num1 in range(10):
+    for num2 in range(10):
+        if num2 > num1 and num1 != 8:
+            print('{}{}'.format(num1, num2), end=", ")
+print("{}{}".format(num1 - 1, num2))
