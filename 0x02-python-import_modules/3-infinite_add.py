f314710a150686a63fc5c079c07090988af3c8e1
sangwani <mightypz@gmail.com>
2021-11-18 23:12:51 +0200

    fix last check fail on 3-infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,7 @@ if __name__ == '__main__':
     import sys
     Sum = 0
     num = sys.argv
-    if len(num) <= 1:
-        print('0')
-    else:
+    if len(num) > 1:
         for i in range(1, len(num)):
             Sum += int(num[i])
     print('{:d}'.format(Sum))
