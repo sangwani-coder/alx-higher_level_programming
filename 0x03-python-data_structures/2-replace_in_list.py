5b7e87a9ae95964b5a192b8ffe5c46453a59ab94
sangwani <mightypz@gmail.com>
2021-11-21 23:39:08 +0200

    Replace element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, new_element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        my_list.insert(idx, new_element)
+        return my_list
