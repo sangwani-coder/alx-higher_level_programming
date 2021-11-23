75f866f0c768267518883b54e294e9394187f833
sangwani <mightypz@gmail.com>
2021-11-23 10:13:30 +0200

    debug 7

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     first = ""
-    if sentence == "":
+    if !(sentence):
         first = None
     first = sentence[0]
     return(length, first)
