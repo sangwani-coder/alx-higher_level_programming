b62af061c7b11c20478b3398b39ffb3369769754
sangwani <mightypz@gmail.com>
2021-11-24 21:17:44 +0200

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    idx_list = [i for i, el in enumerate(my_list) if el == search]
+    for idx in idx_list:
+        new_list[idx] = replace
+    return new_list 
