3b2ae06e7397aa15cbfc329f25f0c85244d95aaa
sangwani <mightypz@gmail.com>
2021-12-06 23:07:58 +0200

    change except condiiton

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = None
     try:
-        result = a / b
-    except:
-        return None
+        a / b
+    except ZeroDivisionError:
+        pass
     finally:
-        print("Inside result: = {}".format(result))
-    return result
+        if b != 0:
+            print("Inside result: = {}".format(result))
+            return a/b
+        else:
+            print("Inside result: None")
+            return None
