e24c024da4d86360f88b115f0a0fa4e326742990
sangwani <mightypz@gmail.com>
2021-12-07 17:20:44 +0200

    fix requirement err

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 class Square:
-
-    """Defines a square object"""
-
+    """Defines a square object
+    """
     def __init__(self, size):
-        """Initializes the size"""
+        """Initializes the size
+
+        Args:
+            param1 (int): size of the square
+        """
         self.__size = size
