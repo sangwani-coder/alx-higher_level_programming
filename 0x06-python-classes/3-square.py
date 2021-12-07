a824c15897cdaae5f837069d70ae8b9e7f2ac641
sangwani <mightypz@gmail.com>
2021-12-07 18:14:47 +0200

    Add module docstring

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that includes Square class"""
+
+
 class Square:
     """defines a square object"""
     def __init__(self, size=0):
@@ -9,6 +12,7 @@ class Square:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
+
     def area(self):
         """calculates and returns the current area"""
         return self.__size * self.__size
