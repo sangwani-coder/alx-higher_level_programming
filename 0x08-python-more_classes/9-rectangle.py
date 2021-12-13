027c7c24a9eb035f697a7cdde4dc8014c77a45e2
sangwani <mightypz2@gmail.com>
2021-12-14 01:26:45 +0200

    debug task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -105,4 +105,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """returns a new rectangle with width and height == size"""
-        return (cls(size, size))
+        return cls(size, size)
