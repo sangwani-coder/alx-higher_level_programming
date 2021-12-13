4ea7f3a99b259d215b4ea6bdb725bba459c6d30b
sangwani <mightypz2@gmail.com>
2021-12-13 07:53:34 +0200

    add function doctest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """ 
 module contains function that adds
-two integers a and b
+two integers a and b and returns the sum
 """
 
 def add_integer(a, b=98):
-    """adds 2 given  integers """
+    """
+        adds 2 given  integers and returns the sum.
+        If a float is passed it should be typecasted to an int
+    """
+
     if (not isinstance(a, int)) and (not isinstance(a, float)):
         raise TypeError("a must be an integer")
     if (not isinstance(b, int)) and (not isinstance(b, float)):
