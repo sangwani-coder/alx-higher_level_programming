eba37a55c50b301bfb1fd93b8f222e151741c946
sangwani <mightypz2@gmail.com>
2021-12-15 12:12:45 +0200

    fix typo in class name

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Defines a locked class LockedClass"""
 
-class LockecClass:
+class LockedClass:
     """ Prevnts a user from dynamically creating new
         instance attributes, except if the new attribute is called
         first_name"""
-    __slots__ = ["first_name"]
+    __slots__ = "first_name"
