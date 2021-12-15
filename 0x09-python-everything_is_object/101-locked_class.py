2380aca3a7f4d476acc8b1977a48c687fd0345e5
sangwani <mightypz2@gmail.com>
2021-12-15 12:04:16 +0200

    add completed optional tasks

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" Defines a locked class LockedClass"""
 
 class LockecClass:
-    pass
+    """ Prevnts a user from dynamically creating new
+        instance attributes, except if the new attribute is called
+        first_name"""
+    __slots__ = ["first_name"]
