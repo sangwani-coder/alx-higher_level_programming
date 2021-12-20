2ed982da7d8175a49067af3a5c9128891e774e94
sangwani <mightypz2@gmail.com>
2021-12-20 20:46:46 +0200

    debug 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,7 +13,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """validates value """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Auth Sangwani P Zyambo
+
+""" Defines a class Rectangle that inherits from Base Geometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """ Derived class that inherits from BaseGeometry base class"""
+
+    def __init__(self, width, height):
+        """instantiates class object"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
