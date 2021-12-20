e5e527d7b80a13dd21b65e5066975145eee1fd58
sangwani <mightypz2@gmail.com>
2021-12-20 21:00:38 +0200

    add return False line

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -11,6 +11,6 @@
 def is_kind_of_class(obj, a_class):
     """ Returns true if object is an instace of a class"""
 
-    if type(obj) == a_class:
+    if isinstance(obj, a_class):
         return True
     return False
