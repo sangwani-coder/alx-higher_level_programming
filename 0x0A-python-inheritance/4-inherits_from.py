b0a1828f21f00e3a35b0f61eaaa778ca11760c12
sangwani <mightypz2@gmail.com>
2021-12-20 21:51:30 +0200

    add type()

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,6 +12,6 @@ def inherits_from(obj, a_class):
     """ returns True if obj is an instance
         of a class or instance of a subclass
     """
-    if issubclass(obj, a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
