0725aee8b6f207e692625bb8370d8a9f802017aa
sangwani <mightypz2@gmail.com>
2021-12-20 20:57:32 +0200

    replace isinstance() with type(), and return False

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -11,5 +11,6 @@
 def is_kind_of_class(obj, a_class):
     """ Returns true if object is an instace of a class"""
 
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
+    return False
