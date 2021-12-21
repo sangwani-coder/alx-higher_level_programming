1a09e50546f44af95ad3f54a926e567556c5812a
sangwani <mightypz2@gmail.com>
2021-12-21 17:33:04 +0200

    add task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Sangwani P Zyambo
+""" Defines a funtion that writes an Object to a text file,
+    using a JSON representation.
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """ writes a python object to a text file,
+        using JSON representation
+        param: (my_obj) - the object to be converted to json
+        param: (filename) - the file to write my_obj to.
+    """
+    import json
+
+    obj = json.dumps(my_obj)
+    with open(filename, 'w') as f:
+        for i in obj:
+            f.write(i)
+
