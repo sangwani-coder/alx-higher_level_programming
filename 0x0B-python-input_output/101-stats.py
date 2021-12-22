9b2ba33d7e0df16d399217fb48b827f27c415e0b
sangwani <mightypz2@gmail.com>
2021-12-22 18:56:01 +0200

    Debug 101 remove trailing whitespace

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Sangwani P Zyambo 
+# Sangwani P Zyambo
 """Reads from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
@@ -17,6 +17,7 @@ def print_stats(size, status_codes):
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
