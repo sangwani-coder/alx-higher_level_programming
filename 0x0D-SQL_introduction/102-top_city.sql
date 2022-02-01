43b8bb3642d8d4500fd4cae0d48bf958809e1d43
Sangwani <mightyp2@gmail.com>
2022-02-01 22:15:44 +0200

    fix typo in file name

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- display the 3 cities with the highest average
+-- temps between July and August
+SELECT `city`, AV(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
