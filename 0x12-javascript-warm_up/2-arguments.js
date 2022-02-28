9ea8136ec8b6e040054ad19d702cccfe1d5280a3
Sangwani <mightyp2@gmail.com>
2022-02-28 10:12:14 +0200

    write task 2 code

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.length < 3) {
+  console.log('No arguments');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
