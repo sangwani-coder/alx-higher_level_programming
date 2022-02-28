35045da0abe827cb8c1c2667a070ac24e521aa55
Sangwani <mightyp2@gmail.com>
2022-02-28 21:45:25 +0200

    write task 10 code

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+function factorial(x) {
+  if (isNaN(x) || x === 0) {
+    return 1;
+} else {
+  return (x * factorial(x - 1));
+ }
+}
+console.log(factorial(num));
