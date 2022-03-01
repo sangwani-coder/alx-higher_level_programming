64982916cc4eeb44d5599f13ebc3231d7155f9c9
Sangwani <mightyp2@gmail.com>
2022-03-01 22:00:23 +0200

    add task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let r = 0; r < this.height; r++) {
+      let row = '';
+      for (let c = 0; c < this.width; c++) row += 'X';
+      console.log(row);
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+};
