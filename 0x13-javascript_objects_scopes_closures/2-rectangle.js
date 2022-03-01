351655ee655529a3daf8933490b3a0e2c67c8ee7
Sangwani <mightyp2@gmail.com>
2022-03-01 21:16:39 +0200

    debug task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || typeof (h) === 'undefined' || typeof (w) === 'undefined') {
-      const newObject = {};
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
