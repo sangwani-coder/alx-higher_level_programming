0afae1f0b4720259332a1c54c5d8d570b9d8385f
Sangwani <mightyp2@gmail.com>
2022-03-04 23:17:54 +0200

    add task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" This module filters all the states from the
+    database hbtn_0e_0_usa that start with upper N.
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """
+        Function containing code to filter all the states
+        from the database.
+    """
+
+    # Create a database connection
+    conn = MySQLdb.connect(
+                host="localhost", port=3306, user=sys.argv[1],
+                passwd=sys.argv[2], db=sys.argv[3], charset="utf8mb4"
+            )
+    cur = conn.cursor()
+    # Select states
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
