3a51feb053cd2b30f2ad97f07cff90a6b93d8bd9
Sangwani <mightyp2@gmail.com>
2022-03-06 16:58:28 +0200

    task 15

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
-# Defines a City model.
-# Inherits from SQLAlchemy Base and links to the MySQL table cities.
+#!/usr/bin/python3i
+""" Defines a City model.
+   Inherits from SQLAlchemy Base and links to the MySQL table cities.
+"""
 
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
