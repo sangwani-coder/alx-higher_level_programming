0793a8f3d25d3029759f5c96b3873ab8fee2ddf2
Sangwani <mightyp2@gmail.com>
2022-03-18 23:43:33 +0200

    debug tasks 4, 5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@
 import requests
 
 if __name__ == "__main__":
-    r = requests.get("http://alx-intranet.hbtn.io/status")
+    r = requests.get("https://alx-intranet.hbtn.io/status")
 
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     url = argv[1]
 
     r = requests.get(url)
-    print(r.headers['X-Request-Id'])
+    print(r.headers.get('X-Request-Id'))
